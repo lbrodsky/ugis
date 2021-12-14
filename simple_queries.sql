032a92c611cc96f29879dd6fd8b420312b3f7076
lbrodsky <lbrodsky@users.noreply.github.com>
2021-12-14 11:39:08 +0100

    first query

--- simple_queries.sql
+++ simple_queries.sql
@@ -1,5 +1,18 @@
 /* 
 Here you find set of simple SQL querries. 
 
+PostgreSQL/PostGIS server running on OSGeo live 9/14 
+db=natural_earth2
+host=localhost
+port=5432
+user / pwd -> OSGeolive readme
+
 */ 
+
+-- Count number of records in DB table ne_10m_populated_places 
+SELECT COUNT(*) 
+FROM ne_10m_populated_places; 
+
+-- Select and print the first record of the table 
+-- TODO 
 
